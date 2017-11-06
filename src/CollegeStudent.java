4627f3bf1693eb22d4ff2ca25b748cc2fd1bbf82
Samantha Rutherford <33076449+srutherford1@users.noreply.github.com>
2017-11-05 21:22:22 -0500

    Update CollegeStudent.java

--- src/CollegeStudent.java
+++ src/CollegeStudent.java
@@ -1,13 +1,15 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Samantha Rutherford
+  	Date: Nov 5th, 2017
 */
 
 public class CollegeStudent {
 
 	public static void main(String[] args) {
-
+		double gpa;
+		String freshman, sophomore, junior, senior;
+		int courseAmount;
 	}
 
 }
