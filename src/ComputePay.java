4aeb16d2e95851872a3044064139ccde4f553926
Samantha Rutherford <33076449+srutherford1@users.noreply.github.com>
2017-11-05 21:41:27 -0500

    Update ComputePay.java

--- src/ComputePay.java
+++ src/ComputePay.java
@@ -1,9 +1,5 @@
-/*
-  	ISYS 320
-  	Name(s):
-  	Date: 
-*/
-
+/**
+Original:
 public class ComputePay {
 	public static void main(String[] args) {
 		System.out.println("My total hours worked:");
@@ -15,4 +11,34 @@ public class ComputePay {
 		System.out.println("My total pay:");
 		System.out.println((4 + 5 + 8 + 4) * 8.75);
 	}
+}
+**/
+/*
+  	ISYS 320
+  	Name(s): Samantha Rutherford
+  	Date: Nov 5th, 2017
+*/
+
+public class ComputePay {
+	public static void main(String[] args) {
+		int hoursWorked = 4 + 5 + 8 + 4;
+		double wages = 8.75;
+		System.out.println("My total hours worked:");
+		System.out.println(hoursWorked);
+
+		System.out.println("My hourly salary:");
+		System.out.println("$" + wages);
+
+		System.out.println("My total pay:");
+		System.out.println("$" + (hoursWorked) * wages);
+	}
+	/**
+		Output:
+				My total hours worked:
+				21
+				My hourly salary:
+				$8.75
+				My total pay:
+				$183.75
+	**/
 }
