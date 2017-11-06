268c09716c682a63ce31f308a03c1e6e706adf03
Samantha Rutherford <33076449+srutherford1@users.noreply.github.com>
2017-11-05 21:29:42 -0500

    Update TestExpressions.java

--- src/TestExpressions.java
+++ src/TestExpressions.java
@@ -1,27 +1,31 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Samantha Rutherford
+  	Date: Nov 5th, 2017
 */
 
 /*
  	Your predicted values for the expressions
-	1.
-	2.
-	3.
-	4.
-	5.
-	6.
-	7.
-	8.
- 
+	1. 2 + 3 * 4 - 6 = 8
+	2. 14 / 7 * 2 + 30 / 5 + 1 = 11
+	3. ( 12 + 6 ) / 3 * 2 = 12
+	4. (238 % 10 + 3 ) % 7 = 4
+	5. 6 * 7 % 4= 2
+	6. 2 + 2 + 3 + 4 = 11
+	7. "2 + 2" + 3 + 4 = ? You cant convert a string to a double?
+	8. 3 + 4 + " 2 + 2" = ?  ^^^^
+
+
  	After testing the expressions in the main method, indicate above if your predictions were correct.
 */
 
-public class TestExpressions {
+public class Tricky {
 
 	public static void main(String[] args) {
-		System.out.println("YOUR_TEST_EXPRESSION");
+		double YOUR_TEST_EXPRESSION = "2 + 2" + 3 + 4;
+		System.out.println(YOUR_TEST_EXPRESSION);
+
 	}
 
 }
+
